864b9d017bedd8be1b5576c569a751088bd3a0c0
RdotSilva <RyandotSilva@gmail.com>
2023-03-10 20:21:40 +0000

    Update logic for filtering genres

--- src/utils/movies/filterMovies.ts
+++ src/utils/movies/filterMovies.ts
@@ -1,4 +1,4 @@
-const filterMoviesByGenre = (genre: string, movies: any): any => {
-    const moviesByGenre = movies.filter((movie: any) => movie.genre === genre);
+export const filterMoviesByGenre = (genre: string, movies: any): any => {
+    const moviesByGenre = movies.filter((movie: any) => movie.genre_ids.includes(genre));
     return moviesByGenre;
 };
