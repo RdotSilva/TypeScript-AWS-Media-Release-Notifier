b5f19e9e1910ce021a4da640246c76dac0e21aba
RdotSilva <RyandotSilva@gmail.com>
2023-03-17 18:48:04 +0000

    Remove unused code

--- src/handlers/apiCall/utils/movies/filterMovies.ts
+++ src/handlers/apiCall/utils/movies/filterMovies.ts
@@ -1,10 +1,4 @@
 export const filterMoviesByGenre = (genres: number[], movies: any): any => {
-    // let arr1 = [1, 2, 3];
-    // let arr2 = [2, 3];
-
-    // let isFounded = arr1.some((ai) => arr2.includes(ai));
-    // const found = arr1.some((r) => arr2.includes(r));
-
     const moviesByGenre = movies.filter((movie: any) => genres.some((genre) => movie.genre_ids.includes(genre)));
     return moviesByGenre;
 };
