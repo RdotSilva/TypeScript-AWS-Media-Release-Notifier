1c1f9fe37a62f4f608e74f839469755f97ebc4c7
RdotSilva <RyandotSilva@gmail.com>
2023-03-19 19:20:31 +0000

    Add filterMoviesByFutureDate function

--- src/handlers/apiCall/utils/movies/filterMovies.ts
+++ src/handlers/apiCall/utils/movies/filterMovies.ts
@@ -2,3 +2,9 @@ export const filterMoviesByGenre = (genres: number[], movies: any): any => {
     const moviesByGenre = movies.filter((movie: any) => genres.some((genre) => movie.genre_ids.includes(genre)));
     return moviesByGenre;
 };
+
+export const filterMoviesByFutureDate = (movieDate: any) => {
+    const givenDate = new Date(movieDate);
+    const currentDate = new Date();
+    return givenDate.getTime() > currentDate.getTime();
+};
