994701e7c6c77f4d8da82c8807cf97043d7ea441
RdotSilva <RyandotSilva@gmail.com>
2023-03-21 20:33:07 +0000

    Add TSDoc comments

--- src/handlers/apiCall/utils/movies/filterMovies.ts
+++ src/handlers/apiCall/utils/movies/filterMovies.ts
@@ -1,9 +1,16 @@
+import { Movie } from '../../types/movie';
+
 export const filterMoviesByGenre = (genres: number[], movies: any): any => {
     const moviesByGenre = movies.filter((movie: any) => genres.some((genre) => movie.genre_ids.includes(genre)));
     return moviesByGenre;
 };
 
-export const filterMoviesByFutureDate = (movies: any) => {
+/**
+ * Filter movies by future release date based on current date
+ * @param movies An array of movies to filter
+ * @returns An array of movies releasing in the future
+ */
+export const filterMoviesByFutureDate = (movies: Movie[]) => {
     const currentDate = new Date();
     const moviesInFuture = movies.filter((movie: any) => {
         const givenDate = new Date(movie.release_date);
