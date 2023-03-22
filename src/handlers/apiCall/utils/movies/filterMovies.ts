ae69dbda16bed8de32625789f5b11aadcd1b114b
RdotSilva <RyandotSilva@gmail.com>
2023-03-22 21:17:31 +0000

    Add TSDoc comment

--- src/handlers/apiCall/utils/movies/filterMovies.ts
+++ src/handlers/apiCall/utils/movies/filterMovies.ts
@@ -1,5 +1,11 @@
 import { Movie } from '../../types/movie';
 
+/**
+ * Filter movies by genres
+ * @param genres An array of genre IDs
+ * @param movies An array of movies
+ * @returns An array of movies filtered by genres
+ */
 export const filterMoviesByGenre = (genres: number[], movies: Movie[]): any => {
     const moviesByGenre = movies.filter((movie: Movie) => genres.some((genre) => movie.genre_ids.includes(genre)));
     return moviesByGenre;
