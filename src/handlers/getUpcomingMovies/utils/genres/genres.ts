d407ef846f27fd43f2a1639aa07821ff54c171a8
RdotSilva <RyandotSilva@gmail.com>
2023-03-26 21:11:59 +0100

    Add TSDoc comments

--- src/handlers/getUpcomingMovies/utils/genres/genres.ts
+++ src/handlers/getUpcomingMovies/utils/genres/genres.ts
@@ -1,3 +1,9 @@
+/**
+ * Map an array of genres to an object
+ * @param array Genre array
+ * @param keyField Key to use in the newly created object
+ * @returns Object with genre codes by genre type
+ */
 export const mapGenresArrayToObject = (array: any, keyField: any) =>
     array.reduce((obj, item) => {
         obj[item[keyField]] = item.id;
