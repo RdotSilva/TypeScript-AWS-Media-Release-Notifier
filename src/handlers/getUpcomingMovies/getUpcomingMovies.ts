c835c0ab9df2a77d1ededcf7428fc6cea13e9c3b
RdotSilva <RyandotSilva@gmail.com>
2023-04-01 17:46:06 +0100

    Refactor using new service

--- src/handlers/getUpcomingMovies/getUpcomingMovies.ts
+++ src/handlers/getUpcomingMovies/getUpcomingMovies.ts
@@ -1,10 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { getUpcomingMoviesByGenres } from './services/getUpcomingMovies';
-import { CloneReceiptRuleSetCommand, SendEmailCommand } from '@aws-sdk/client-ses';
-
-import { sesClient } from './libs/sesClient';
-
-const { SOURCE_EMAIL } = process.env;
+import { sendOutgoingEmail } from './services/sendOutgoingEmail';
 
 /**
  * Fetch upcoming movies based on genres
@@ -16,32 +12,10 @@ const { SOURCE_EMAIL } = process.env;
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     let genres;
 
-    // SES logic
-    const params = {
-        Destination: {
-            ToAddresses: ['todo@todo.com'],
-        },
-        // Interpolate the data in the strings to send
-        Message: {
-            Body: {
-                Text: {
-                    Data: `New Release Data Goes Here`,
-                },
-            },
-            Subject: { Data: `Release Notifier - New Release Found!` },
-        },
-        Source: SOURCE_EMAIL,
-    };
-
-    const sendCommand = new SendEmailCommand(params);
-
     try {
-        await sesClient.send(sendCommand);
-    } catch (error) {
-        console.log(`Unable to send email: ${JSON.stringify(error)}`);
-    }
+        // Send email test
+        await sendOutgoingEmail();
 
-    try {
         if (event.body) {
             let body = JSON.parse(event.body);
             genres = body.genres;
