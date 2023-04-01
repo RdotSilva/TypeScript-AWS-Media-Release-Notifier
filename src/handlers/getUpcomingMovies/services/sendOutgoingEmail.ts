019495487042f0038de7eaec5361b2a40ec9eb68
RdotSilva <RyandotSilva@gmail.com>
2023-04-01 17:45:57 +0100

    Add new service for sending email

---
+++ src/handlers/getUpcomingMovies/services/sendOutgoingEmail.ts
@@ -0,0 +1,32 @@
+import { SendEmailCommand } from '@aws-sdk/client-ses';
+
+import { sesClient } from '../libs/sesClient';
+
+const { SOURCE_EMAIL } = process.env;
+
+// SES logic
+const params = {
+    Destination: {
+        ToAddresses: ['todo@todo.com'],
+    },
+    // Interpolate the data in the strings to send
+    Message: {
+        Body: {
+            Text: {
+                Data: `New Release Data Goes Here`,
+            },
+        },
+        Subject: { Data: `Release Notifier - New Release Found!` },
+    },
+    Source: SOURCE_EMAIL,
+};
+
+export const sendOutgoingEmail = async () => {
+    const sendCommand = new SendEmailCommand(params);
+
+    try {
+        await sesClient.send(sendCommand);
+    } catch (error) {
+        console.log(`Unable to send email: ${JSON.stringify(error)}`);
+    }
+};
