c8f6fad1340bb904983b8b98494834eeec781f58
RdotSilva <RyandotSilva@gmail.com>
2023-04-02 22:04:35 +0100

    Add TSDoc comments

--- src/handlers/getUpcomingMovies/libs/sesClient.ts
+++ src/handlers/getUpcomingMovies/libs/sesClient.ts
@@ -1,6 +1,10 @@
 import { SESClient } from '@aws-sdk/client-ses';
 
 const region = process.env.REGION;
+
+/**
+ * Client used for AWS SES to send emails
+ */
 const sesClient = new SESClient({ region: region });
 
 export { sesClient };
