55c3d859602284b4fb9f30b75d705a491e9e7a09
RdotSilva <RyandotSilva@gmail.com>
2023-05-08 21:04:37 +0100

    Add updateUser function

--- src/handlers/updateUserOptions/services/updateUser.ts
+++ src/handlers/updateUserOptions/services/updateUser.ts
@@ -1,1 +1,28 @@
-// TODO: Add DynamoDB logic in here
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
+
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+const updateUser = async (data: any) => {
+    const params = {
+        TableName: process.env.DYNAMODB_TABLE,
+        Item: {
+            id: uuid.v1(),
+            name: data.name,
+            description: data.description,
+        },
+    };
+
+    try {
+        await dynamo.put(params).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: 'User options updated successfully' }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Error updating user options' }),
+        };
+    }
+};
